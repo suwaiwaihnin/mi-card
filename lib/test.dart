dcbc4015554e36e5aa8017ec1f08e80380c007cc
Su Wai Wai Hnin <suwaiwaihnin20@gmail.com>
2020-12-19 21:41:54 +0630

    add code to test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -10,7 +10,36 @@
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//         // Text('Hello Flutter'),
-//         );
+//       home: Scaffold(
+//         appBar: AppBar(
+//           leading: Icon(Icons.thumb_up),
+//           title: Text('Hello Flutter'),
+//           centerTitle: false,
+//         ),
+//         body: Container(
+//           decoration: BoxDecoration(
+//               color: Colors.green,
+//               // borderRadius: BorderRadius.circular(20),
+//               borderRadius: BorderRadius.only(
+//                 topLeft: Radius.circular(20),
+//               ),
+//               boxShadow: [
+//                 BoxShadow(
+//                   offset: Offset(10, 10),
+//                   color: Colors.grey,
+//                   blurRadius: 5,
+//                 ),
+//               ]),
+//           alignment: Alignment.center,
+//           padding: EdgeInsets.all(20.0),
+//           child: Text(
+//             'Hello flutter from learning flutter',
+//             style: TextStyle(
+//               fontSize: 15.0,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
 //   }
 // }
